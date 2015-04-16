106155fe1fceab2e1ad4f5d9173fa4f61e6ee73a
ITkorea <davisasan@hanmail.net>
2015-04-17 01:03:15 +0900

    게시글과 댓글의 작성자 정보 추가하기

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = ["한식","일식","중식","양식"]
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = User.new
+u.username = "charles"
+u.password = "12345678"
+u.save
+
+u = User.new
+u.username = "robert"
+u.password = "87654321"
+u.save
